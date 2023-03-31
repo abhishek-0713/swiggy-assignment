f9c8fc3f4ff1376dfcf27a55d0370fbdf4494547
Coder Abhi <coder.abhi0713@gmail.com>
2023-03-31 17:05:29 +0530

    application ready to run

--- multiplayer-card-game-assignment/src/Main.java
+++ multiplayer-card-game-assignment/src/Main.java
@@ -5,5 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of players: ");
+        int numPlayers = scanner.nextInt();                        // Input Number of Players
+        scanner.nextLine();
+
+        List<String> playerNames = new ArrayList<>();
+        for (int i = 1; i <= numPlayers; i++) {
+            System.out.print("Enter name of player " + i + ": ");
+            String playerName = scanner.nextLine();                // Input Player Name
+            playerNames.add(playerName);
+        }
+
+
+//        Initialize and Start the Game
+
+        Game game = new Game();
+
+        game.startGame(playerNames);
+        game.playGame();
+        game.endGame();
+
+        scanner.close();
     }
 }
